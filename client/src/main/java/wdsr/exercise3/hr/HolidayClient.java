35793ec88a76d64d1fe5cdbcf872565bc87332f8
adam.jakomulski <adam.jakomulski@WAJAKOMULSKI.BMSGRP.local>
2017-04-20 13:18:16 +0200

    done

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -3,31 +3,58 @@ package wdsr.exercise3.hr;
 import java.net.URL;
 import java.util.Date;
 
+import wdsr.exercise3.ws.EmployeeType;
+import wdsr.exercise3.ws.HolidayRequest;
+import wdsr.exercise3.ws.HolidayType;
+import wdsr.exercise3.ws.HumanResource;
+import wdsr.exercise3.ws.HumanResourceService;
+
 // TODO Complete this class to book holidays by issuing a request to Human Resource web service.
 // In order to see definition of the Human Resource web service:
 // 1. Run HolidayServerApp.
 // 2. Go to http://localhost:8090/holidayService/?wsdl
 public class HolidayClient {
+	private final HumanResource port;
+
 	/**
 	 * Creates this object
-	 * @param wsdlLocation URL of the Human Resource web service WSDL
+	 * 
+	 * @param wsdlLocation
+	 *            URL of the Human Resource web service WSDL
 	 */
 	public HolidayClient(URL wsdlLocation) {
-		// TODO
+		port = new HumanResourceService(wsdlLocation).getHumanResourcePort();
 	}
-	
+
 	/**
 	 * Sends a holiday request to the HumanResourceService.
-	 * @param employeeId Employee ID
-	 * @param firstName First name of employee
-	 * @param lastName Last name of employee
-	 * @param startDate First day of the requested holiday
-	 * @param endDate Last day of the requested holiday
+	 * 
+	 * @param employeeId
+	 *            Employee ID
+	 * @param firstName
+	 *            First name of employee
+	 * @param lastName
+	 *            Last name of employee
+	 * @param startDate
+	 *            First day of the requested holiday
+	 * @param endDate
+	 *            Last day of the requested holiday
 	 * @return Identifier of the request, if accepted.
-	 * @throws ProcessingException if request processing fails.
+	 * @throws ProcessingException
+	 *             if request processing fails.
 	 */
-	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
+	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate)
+			throws ProcessingException {
+
+		EmployeeType employeeType = new EmployeeType(employeeId, firstName, lastName);
+		HolidayType holidayType = new HolidayType(startDate, endDate);
+
+		HolidayRequest holidayRequest = new HolidayRequest(holidayType, employeeType);
+
+		try {
+			return port.holiday(holidayRequest).getRequestId();
+		} catch (RuntimeException e) {
+			throw new ProcessingException(e);
+		}
 	}
 }
---
+++ client/src/main/java/wdsr/exercise3/ws/HolidayType.java
@@ -0,0 +1,95 @@
+
+package wdsr.exercise3.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import java.util.Date;
+
+/**
+ * <p>
+ * Java class for HolidayType complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType name="HolidayType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="StartDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *         &lt;element name="EndDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "HolidayType", propOrder = { "startDate", "endDate" })
+public class HolidayType {
+
+	public HolidayType() {
+	}
+
+	public HolidayType(Date startDate, Date endDate) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	@XmlElement(name = "StartDate", required = true)
+	@XmlSchemaType(name = "date")
+	protected Date startDate;
+	@XmlElement(name = "EndDate", required = true)
+	@XmlSchemaType(name = "date")
+	protected Date endDate;
+
+	/**
+	 * Gets the value of the startDate property.
+	 * 
+	 * @return possible object is {@link Date }
+	 * 
+	 */
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	/**
+	 * Sets the value of the startDate property.
+	 * 
+	 * @param value
+	 *            allowed object is {@link Date }
+	 * 
+	 */
+	public void setStartDate(Date value) {
+		this.startDate = value;
+	}
+
+	/**
+	 * Gets the value of the endDate property.
+	 * 
+	 * @return possible object is {@link Date }
+	 * 
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * Sets the value of the endDate property.
+	 * 
+	 * @param value
+	 *            allowed object is {@link Date }
+	 * 
+	 */
+	public void setEndDate(Date value) {
+		this.endDate = value;
+	}
+
+}
